470935800e5eda2a32eaa99a10a2c5110b3894e0
DESKTOP-D8K8NF9\Abele <abele@e3d.rwth-aachen.de>
2023-10-31 15:10:32 +0100

    Dictionaries to shorten some questions and answers
    Use this to make some answers easier to display and to understand.

---
+++ PythonCode/Shortened_Answers_Titles.py
@@ -0,0 +1,68 @@
+# Short answers
+q_242 = {
+    "Implementation phase - Installation of sensors, etc.": "Implementation Phase",
+    "Analysis phase - completion of data collection and analysis underway": "Analysis phase",
+    "Data collection phase - the case study data collection is currently underway": "Data collection phase",
+    "Completed with publication - the case study has been completed and published (you can submit a link in a future question)": "Completed with publication"
+}
+
+q_47 = {
+    "Full time (stable occupant of the building)": "Full time"
+}
+
+q_50 = {
+    "Thermostats (e.g. limiting changes, e.g, to +/-2°C)": "Thermostats"
+}
+
+q_158 = {
+    "DOAS with VRF serving radiant heaters and chilled beams": "DOAS with VRF serving radiant heaters",
+    "Concrete core tempering/component activation": "Concrete core activation",
+    "VAV AHU system without zone reheat or perimeter heating": "VAV AHU system without zone reheat",
+    "VAV AHU system with zone reheat or perimeter heating": "VAV AHU system with zone reheat"
+}
+
+q_176 = {
+    "Mixed-mode with motorized windows automatically controlled by the BAS": "Mixed-Mode with motorized windows",
+    "Regulated mixed-mode (mechanical ventilation rate of a room changes when a window is open)": "Regulated mixed-mode",
+    "Unregulated mixed-mode (opening windows in a room does not affect mechanical ventilation)": "Unregulated mixed-mode"
+}
+
+q_179 = {
+    "Discharge air temperature (i.e., air temperature at supply diffuser)": "Discharge air temperature"
+}
+
+q_238 = {
+    "Controllable thermostats (not those that display numbers and do nothing else)": "Controllable thermostats"
+}
+
+q_ID22 = {
+    "Data about the building operators or operations policies for non-residential case studies": "Building operators",
+    "Building lighting systems and automation and/or controls information": "Building lighting systems/automation/controls",
+    "Development of machine learning models to predict human behavior, preference, or satisfaction": "predict human behavior/preference/satisfaction",
+    "Data about occupant interaction interfaces and motivation": "Occupant interaction interfaces/motivation",
+    "An intervention or experiment that was designed to improve building performance or occupant satisfaction": "improve building performance/occupant satisfaction",
+    "Building HVAC and automation and/or controls information": "Building HVAC/automation/controls",
+    "Modes of occupant detection, presence and/or indoor environmental sensing": "Occupant detection/presence/indoor environmental sensing",
+    "Data about the occupants (occupant demographics and/or meta data)": "Occupant demographics/meta data"
+}
+
+short_replies = {
+    "Q242": q_242,
+    "Q47": q_47,
+    "Q50": q_50,
+    "Q158": q_158,
+    "Q176": q_176,
+    "Q179": q_179,
+    "Q238": q_238,
+    "QID22": q_ID22
+}
+
+# Titles
+short_titles = {
+    "QID22": "Categories of analysis",
+    "Q167": "Spatial scope of the study",
+    "Q168": "Approximate gross floor area (GFA) in square meters (all spaces and buildings)",
+    "Q248": "Number of participants",
+    "Q155": "What is the buildings main use?",
+}
+
